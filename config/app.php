da61f5ac5e0b81f4724a8c57d9cdd7765b33d8a5
ClaytonKinnane <ClaytonKinnane04186@hotmail.com>
2026-07-24 17:54:59 +0300

    feat: add starter application configuration

---
+++ config/app.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'name' => 'АСУ-ВЧ',
+    'version' => '0.1.0-dev',
+    'environment' => 'local',
+    'debug' => true,
+    'timezone' => 'Europe/Amsterdam',
+    'theme' => 'asu-blue',
+    'session_name' => 'asu_vch_session',
+];
