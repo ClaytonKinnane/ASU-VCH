799b1304e11ae29c3f55a64e1290ab1f36831c3e
ClaytonKinnane <ClaytonKinnane04186@hotmail.com>
2026-08-03 08:34:36 +0300

    fix(reference): repair catalog source triggers

--- database/MilitaryRankDirectoryV2/triggers-e.sql
+++ database/MilitaryRankDirectoryV2/triggers-e.sql
@@ -2,8 +2,23 @@ CREATE TRIGGER trg_military_rank_version_sources_before_insert
 BEFORE INSERT ON military_rank_catalog_version_sources
 FOR EACH ROW
 BEGIN
-    IF COALESCE((SELECT lifecycle_status FROM military_rank_catalog_versions WHEQHYH‘UЛШ][ЩЧЭ™\њЪ[Ы—ЪY
-K	ЙКH€	ШќZ[[™ЙИS‚€ТQУђSФSХUH	НL	ИСUQTФРQСWХVH	УRSUT–WФђS’ЧХ‘T”ТSУ—ФУХTђСWТS”СT•С“Фђ’QS‰ОВ€S‘QЋВ‘S‘В‚ђФ‘PUH’QСССT€™ЧЫZ[]\ћWЬ[љЧЭ™\њЪ[Ы—ЬЫЭ\Щ\ЧШ™Y›Ь™WЭ\]Bђ‘Q“Ф‘HTUHУ€Z[]\ћWЬ[љЧШШ][ЩЧЭ™\њЪ[Ы—ЬЫЭ\Щ\В‘“Ф€PPТ“ХВђ‘QТS‚€Q€УРSTРСJ
-СSPХY™XЮXЫWЬЭ]\И”“УHZ[]\ћWЬ[љЧШШ][ЩЧЭ™\њЪ[ЫњИТT‘HYHУШ][ЩЧЭ™\њЪ[Ы—ЪY
-K	ЙКH€	ШќZ[[™ЙВ€Ф€‘UЛШ][ЩЧЭ™\њЪ[Ы—ЪY€УШ][ЩЧЭ™\њЪ[Ы—ЪYS‚€ТQУђSФSХUH	НL	ИСUQTФРQСWХVH	УRSUT–WФђS’ЧХ‘T”ТSУ—ФУХTђСWХTUWС“Фђ’QS‰ОВ€S‘QЋВ‘S‘В‚ђФ‘PUH’QССT€™ЧЫZ[]\ћWЬ[љЧЭ™\њЪ[Ы—ЬЫЭ\Щ\ЧШ™Y›Ь™WЩ[]Bђ‘Q“Ф‘HSUHУ€Z[]\ћWЬ[љЧШШ][ЩЧЭ™\њЪ[Ы—ЬЫЭ\Щ\В‘“Ф€PPТ“ХВђ‘QТS‚€Q€УРSTРСJ
-СSPХY™XЮXЫWЬЭ]\И”“УHZ[]\ћWЬ[љЧШШ][ЩЧЭ™\њЪ[ЫњИТTR–BТфДBж6FЖцuчfW'6–цец–B’Вrr’Гвv'V–ЖF–жrrD„Tаў4”tдВ5Е5DDRsCSr4UBФU54tUхDU…BТtФ”Д•D%•х$дµхdU%4”фех4хU$4UфDTДUDUфdх$$”DDTвs°ўTдB”c°¤TдC°  
+    IF COALESCE((SELECT lifecycle_status FROM military_rank_catalog_versions WHERE id = NEW.catalog_version_id), '') <> 'building' THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'MILITARY_RANK_VERSION_SOURCE_INSERT_FORBIDDEN';
+    END IF;
+END;
+
+CREATE TRIGGER trg_military_rank_version_sources_before_update
+BEFORE UPDATE ON military_rank_catalog_version_sources
+FOR EACH ROW
+BEGIN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'MILITARY_RANK_VERSION_SOURCE_UPDATE_FORBIDDEN';
+END;
+
+CREATE TRIGGER trg_military_rank_version_sources_before_delete
+BEFORE DELETE ON military_rank_catalog_version_sources
+FOR EACH ROW
+BEGIN
+    IF COALESCE((SELECT lifecycle_status FROM military_rank_catalog_versions WHERE id = OLD.catalog_version_id), '') <> 'building' THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'MILITARY_RANK_VERSION_SOURCE_DELETE_FORBIDDEN';
+    END IF;
+END;
